30a9e3c658c89df47aefc31e4f353a815e4a2183
chelopereira <chelopp87@gmail.com>
2017-01-07 01:14:08 -0500

    author model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107054818) do
+ActiveRecord::Schema.define(version: 20170107060700) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
